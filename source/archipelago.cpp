d772c27467dbff255614b93e1f535c16b76c9752
BuffYoda21 <buffyoda21@gmail.com>
2026-09-25 12:15:51 -0500

    Added support for larger payloads

--- source/archipelago.cpp
+++ source/archipelago.cpp
@@ -6,35 +6,38 @@
 // last checked address: 0x510000
 // Still need a better solution to do this since this will change pretty often
 APHeader g_apHeader = {};
+static std::string g_apPayload;
 
 void ConnectAndGenerate() {
+    g_apHeader.stage      = 0;
+    g_apHeader.bufferSize = 0;
+    g_apPayload.clear();
     g_apHeader.listening = 1;
     printf("\x1b[20;0HWaiting for proxy to send data...\n");
 }
 
 void CancelArchipelago() {
-    g_apHeader.listening   = 0;
-    g_apHeader.lock        = 0;
-    g_apHeader.payloadSize = 0;
-    memset(g_apHeader.payload, 0, sizeof(g_apHeader.payload));
+    g_apHeader.listening  = 0;
+    g_apHeader.lock       = 0;
+    g_apHeader.stage      = 0;
+    g_apHeader.bufferSize = 0;
+    memset(g_apHeader.buffer, 0, sizeof(g_apHeader.buffer));
+    g_apPayload.clear();
 }
 
 void PollArchipelagoPayload() {
     if (g_apHeader.listening == 0 || g_apHeader.lock == 0)
         return;
 
-    uint32_t payloadSize = g_apHeader.payloadSize;
+    uint32_t payloadSize = g_apHeader.bufferSize;
     ClearDescription(); // hacky way to select the top screen
 
-    if (payloadSize >= sizeof(g_apHeader.payload)) {
+    if (payloadSize > sizeof(g_apHeader.buffer)) {
         printf("\x1b[20;0HError: Invalid AP payload size: %lu\n", static_cast<unsigned long>(payloadSize));
         return;
     }
 
-    static char data[sizeof(g_apHeader.payload) + 1];
-
-    memcpy(data, g_apHeader.payload, payloadSize);
-    data[payloadSize] = '\0';
+    g_apPayload.append(g_apHeader.buffer, payloadSize);
 
     // printf("%s\n\n", data);
     printf("Received AP payload: %ld bytes\n", payloadSize);
@@ -43,5 +46,11 @@ void PollArchipelagoPayload() {
     // data has been copied out of the buffer, so it's safe to delete it now
     g_apHeader.lock = 0;
 
-    LoadPresetRaw(data, OptionCategory::Setting);
+    if (g_apHeader.stage == 1) {
+        if (LoadPresetRaw(g_apPayload, OptionCategory::Setting))
+            printf("Settings loaded successfully!\n");
+        else
+            printf("ERROR: Failed to load settings\n");
+        g_apPayload.clear();
+    }
 }
--- source/archipelago.hpp
+++ source/archipelago.hpp
@@ -4,13 +4,13 @@
 
 #pragma pack(push, 1)
 struct APHeader {
-    char identifier[8]      = { 'O', 'O', 'T', '3', 'R', 'A', 'N', 'D' };
-    uint8_t version         = 4;
-    uint8_t listening       = 0; // 0 = no, 1 = yes
-    uint8_t lock            = 0; // 0 = safe for proxy to write, 1 = safe for client to read
-    uint8_t stage           = 0; // 0 = slot data, 1 = item->location mapping
-    uint32_t payloadSize    = 0;
-    char payload[64 * 1024] = {};
+    char identifier[8]     = { 'O', 'O', 'T', '3', 'R', 'A', 'N', 'D' };
+    uint8_t version        = 4;
+    uint8_t listening      = 0; // 0 = no, 1 = yes
+    uint8_t lock           = 0; // 0 = safe for proxy to write, 1 = safe for client to read
+    uint8_t stage          = 0; // 0 = slot data, 1 = item->location mapping
+    uint32_t bufferSize    = 0;
+    char buffer[64 * 1024] = {};
 };
 #pragma pack(pop)
 
